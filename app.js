ff9cc2a9b53e7e8fd8e2e059ae6b643ed493ae35
David T. <davidt.info@gmail.com>
2024-09-27 11:19:16 -0300

    Break

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ while (chute != numeroSecreto) {
         chute = prompt('Escolha um número entre 1 e 10');
         // se chute for igual ao número secreto
         if (chute == numeroSecreto) {
-                alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas!`);
+                break;
+                
         } else {
                 if (chute > numeroSecreto) {
                 alert(`O número secreto é menor que ${chute}`);
@@ -17,5 +18,11 @@ while (chute != numeroSecreto) {
                 }
         }
         tentativas++;
+}
+
+if (tentativas > 1) {
+        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas!`);
+} else {
+        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa!`); 
 }
 
