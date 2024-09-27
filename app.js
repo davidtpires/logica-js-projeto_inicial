63ceb59c86423e1c01b0cfb2c8d3690e94bf1103
David T. <davidt.info@gmail.com>
2024-09-27 10:53:40 -0300

    Enquanto...

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 5;
-console.log('numeroSecreto')
-let chute =prompt('Escolha um número entre 1 e 10');
+console.log(numeroSecreto)
+let chute;
 
-// se chute for igual ao número secreto
-if (chute == numeroSecreto) {
-        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
-} else {
-        if (chute > numeroSecreto) {
-                alert(`O número secreto é menor que ${chute}`);
+while (chute != numeroSecreto) {
+        chute = prompt('Escolha um número entre 1 e 100');
+        // se chute for igual ao número secreto
+        if (chute == numeroSecreto) {
+            alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
         } else {
+            if (chute > numeroSecreto) {
+                alert(`O número secreto é menor que ${chute}`);
+            } else {
                 alert(`O número secreto é maior que ${chute}`);
+            }
         }
 }
