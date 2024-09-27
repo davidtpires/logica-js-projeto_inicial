fad1cf9529c80e201eae56427dfd2d2121be39bc
David T. <davidt.info@gmail.com>
2024-09-27 10:58:30 -0300

    Tentativas

--- app.js
+++ app.js
@@ -2,17 +2,20 @@ alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 5;
 console.log(numeroSecreto)
 let chute;
+let tentativas = 1;
 
 while (chute != numeroSecreto) {
-        chute = prompt('Escolha um número entre 1 e 100');
+        chute = prompt('Escolha um número entre 1 e 10');
         // se chute for igual ao número secreto
         if (chute == numeroSecreto) {
-            alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
+                alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas!`);
         } else {
-            if (chute > numeroSecreto) {
+                if (chute > numeroSecreto) {
                 alert(`O número secreto é menor que ${chute}`);
-            } else {
+                } else {
                 alert(`O número secreto é maior que ${chute}`);
-            }
+                }
         }
+        tentativas++;
 }
+
