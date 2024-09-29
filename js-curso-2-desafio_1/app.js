001d1b56910a521ee2fffd9a2684f410b0f456c4
DavidT <davidt.info@gmail.com>
2024-09-29 11:47:22 -0300

    Desafio: hora da prática

--- js-curso-2-desafio_1/app.js
+++ js-curso-2-desafio_1/app.js
@@ -1,22 +1,59 @@
-let titulo = document.querySelector('h1')
-titulo.innerHTML = 'Hora do Desafio.'
+//Desafio 1:
+// let titulo = document.querySelector('h1')
+// titulo.innerHTML = 'Hora do Desafio.'
 
-function verificarConsole() {
-    console.log('O botão foi clicado');
+// function verificarConsole() {
+//     console.log('O botão foi clicado');
+// }
+
+// function verificarAlert() {
+//     alert('Eu amo JS');
+// }
+
+// function verificarPrompt() {
+//     let nomeCidade = prompt('Informe o nome de uma cidade do Brasil');
+//     alert(`Estive em ${nomeCidade} e lembrei de você.`);
+// }
+
+// function verificarSoma() {
+//     let nr1 = parseInt(prompt('Informe o 1º nr:'));
+//     let nr2 = parseInt(prompt('Informe o 2º nr:'));
+//     let soma = nr1 + nr2;
+//     alert(`A soma de ${nr1} + ${nr2} é igual a ${soma}`)
+// }
+
+//Desafio 2
+function olaMundo(){
+    console.log('Olá mundo');
+}
+olaMundo();
+
+function olaUsuario(nome) {
+    console.log(`Olá, ${nome}`);
 }
 
-function verificarAlert() {
-    alert('Eu amo JS');
+olaUsuario('David');
+
+function dobroNr(nr){
+    let dobro = parseInt(nr)*2;
+    return dobro;
 }
 
-function verificarPrompt() {
-    let nomeCidade = prompt('Informe o nome de uma cidade do Brasil');
-    alert(`Estive em ${nomeCidade} e lembrei de você.`);
-}
+console.log(dobroNr(5));
 
-function verificarSoma() {
-    let nr1 = parseInt(prompt('Informe o 1º nr:'));
-    let nr2 = parseInt(prompt('Informe o 2º nr:'));
-    let soma = nr1 + nr2;
-    alert(`A soma de ${nr1} + ${nr2} é igual a ${soma}`)
+function calcularMedia(nr1, nr2, nr3){
+    let media = (parseInt(nr1) + parseInt(nr2) + parseInt(nr3)) / 3;
+    return media;
 }
+console.log(calcularMedia(5,6,7));
+
+function encontraNrMaior(nr1, nr2){
+    return nr1 > nr2 ? nr1 : nr2;
+}
+console.log(encontraNrMaior(5,6));
+
+
+function quadrado(nr){
+    return nr * nr;
+}
+console.log(quadrado(5));
