07ac8101bdc00fcaaaa73e39ef57e39c6b12a2f6
DavidT <davidt.info@gmail.com>
2024-09-29 11:33:52 -0300

    Tipo booleano

--- app.js
+++ app.js
@@ -9,9 +9,10 @@ exibirTextoNaTela('h1', 'Jogo do número secreto');
 exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 
 function verificarChute() {
-    console.log(numeroSecreto)
+    let chute = document.querySelector('input').value;
+    console.log(chute == numeroSecreto);
 }
 
 function gerarNumeroAleatorio() {
-    return parseInt(Math.random() * 10 + 1)
+    return parseInt(Math.random() * 10 + 1);
 }
